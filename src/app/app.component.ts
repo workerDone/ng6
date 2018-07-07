73a7ee219950d8af634a705399d76268fedb4205
Mykola <chmutkola@gmail.com>
2018-07-07 19:27:17 +0300

    add environment, settings to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  ngOnInit() {
+    console.log(environment.url)
+  }
+  
 }
